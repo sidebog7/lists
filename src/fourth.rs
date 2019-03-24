b4b321d216e3c959d1092ec1b881d177fcbd8f13
Gareth Pendleton <gareth.sidebottom@gmail.com>
2019-03-24 16:18:54 +0000

    Added push and pop back and tests

--- src/fourth.rs
+++ src/fourth.rs
@@ -62,6 +62,36 @@ impl<T> List<T> {
         })
     }
 
+    pub fn push_back(&mut self, elem: T) {
+        let new_tail = Node::new(elem);
+        match self.tail.take() {
+            Some(old_tail) => {
+                old_tail.borrow_mut().next = Some(new_tail.clone());
+                new_tail.borrow_mut().prev = Some(old_tail);
+                self.tail = Some(new_tail);
+            }
+            None => {
+                self.head = Some(new_tail.clone());
+                self.tail = Some(new_tail);
+            }
+        }
+    }
+
+    pub fn pop_back(&mut self) -> Option<T> {
+        self.tail.take().map(|old_tail| {
+            match old_tail.borrow_mut().prev.take() {
+                Some(new_tail) => {
+                    new_tail.borrow_mut().next.take();
+                    self.tail = Some(new_tail);
+                }
+                None => {
+                    self.head.take();
+                }
+            }
+            Rc::try_unwrap(old_tail).ok().unwrap().into_inner().elem
+        })
+    }
+
     pub fn peek_front(&self) -> Option<Ref<T>> {
         self.head
             .as_ref()
@@ -83,6 +113,7 @@ mod test {
     fn basics() {
         let mut list = List::new();
 
+        // --- Front ---
         // Check empty list
         assert_eq!(list.pop_front(), None);
 
@@ -103,6 +134,26 @@ mod test {
 
         assert_eq!(list.pop_front(), Some(1));
         assert_eq!(list.pop_front(), None);
+
+        // --- Back ---
+        // Check empty list
+        assert_eq!(list.pop_back(), None);
+
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+
+        assert_eq!(list.pop_back(), Some(3));
+        assert_eq!(list.pop_back(), Some(2));
+
+        list.push_back(4);
+        list.push_back(5);
+
+        assert_eq!(list.pop_back(), Some(5));
+        assert_eq!(list.pop_back(), Some(4));
+
+        assert_eq!(list.pop_back(), Some(1));
+        assert_eq!(list.pop_back(), None);
     }
 
     #[test]
